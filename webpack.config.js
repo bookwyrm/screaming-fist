fb6d3da161d14dc9de522cd1cd478d0eb81d23a2
Matt Vanderpol <matt@vanderpol.net>
2017-12-21 06:33:06 -0800

    Basic Sass setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 const path = require('path')
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+
+const extractSass = new ExtractTextPlugin({
+  filename: "[name].[contenthash].css"
+})
 
 module.exports = {
-  entry: './src/js/index.js',
+  entry: [
+    './src/js/index.js',
+    './sass/style.scss'
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  devtool: "source-map", // any "source-map"-like devtool is possible
   module: {
     rules: [
       {
@@ -14,7 +23,30 @@ module.exports = {
           'style-loader',
           'css-loader'
         ]
+      },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [
+            {
+              // translates CSS into CommonJS
+              loader: "css-loader", options: {
+                sourceMap: true
+              }
+            },
+            {
+              // compiles Sass to CSS
+              loader: "sass-loader", options: {
+                sourceMap: true
+              }
+            }
+          ],
+          fallback: "style-loader"
+        })
       }
     ]
-  }
+  },
+  plugins: [
+    extractSass
+  ]
 };

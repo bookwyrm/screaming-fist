12fec422b78a06b05c45580c342b91710cfa3ed4
Matt Vanderpol <matt@vanderpol.net>
2018-03-08 06:19:44 -0800

    Cleanup JS generated from style entry point

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,12 +2,17 @@ const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 const ExtractTextPlugin = require("extract-text-webpack-plugin")
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const ExtraneousFileCleanupPlugin = require('webpack-extraneous-file-cleanup-plugin');
 const autoprefixer = require('autoprefixer')
 const cssnano = require('cssnano')
 
 module.exports = merge(common, {
   plugins: [
     new ExtractTextPlugin({filename: "style-prod.css"}),
+    new ExtraneousFileCleanupPlugin({
+      extensions: [ '.js' ],
+      minBytes: 500
+    }),
     new UglifyJSPlugin()
   ],
   module: {

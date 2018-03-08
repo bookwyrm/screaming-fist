f2f04eab8b5d04ac675c77a6d9b26172078a18f0
Matt Vanderpol <matt@vanderpol.net>
2018-03-08 06:11:29 -0800

    Drop trailing comma

--- webpack.common.js
+++ webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
     style: './sass/style.scss'
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(['dist'])
   ],
   output: {
     filename: '[name].js',
